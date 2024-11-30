4ffb3a9bc057a0bfefa8415d147a1643c552bfda
Jason Summers <jason1@pobox.com>
2024-11-30 09:30:41 -0500

    Update "cp932" tools to support Shift_JIS-2004

--- cp932/cp932.py
+++ cp932/cp932.py
@@ -14,25 +14,85 @@ import sys
 import re
 import zlib
 
+g_num_src_codes = 65536
+g_num_dst_codes = 32768*3
+
 class context:
     def __init__(ctx):
         ctx.debugmode = False
         ctx.debugmode2 = False
+        ctx.use_xor_filter = False
         ctx.re1 = re.compile(r'([0-9a-zA-Z]+)[ \t]+([0-9a-zA-Z]+)[ \t]')
+        ctx.re2 = re.compile(r'([0-9a-zA-Z]+)[ \t]+U\+([0-9a-fA-F]+)[ \t]')
+        ctx.re_1to2 = re.compile(r'([0-9a-zA-Z]+)[ \t]+U\+([0-9a-fA-F]+)\+([0-9a-fA-F]+)[ \t]')
+        ctx.re_undef = re.compile(r'([0-9a-zA-Z]+)[ \t]+#')
+        # These are the codepoints that can be the second of two
+        # Unicode codepoints that a single Shift JIS code maps to.
+        ctx.hack_1to2_map = { 0x02e5: 0xf0, 0x02e9: 0xf1, 0x0300: 0xf2, \
+            0x0301: 0xf3, 0x309a: 0xf4 }
+
+def map_1to2_hack(ctx, w2, w3):
+    return (ctx.hack_1to2_map[w3]<<16) + w2
 
 def read_mapping_file(ctx):
-    for line in ctx.inf:
+    for line1 in ctx.inf:
+        line = line1.rstrip('\n')
+        flag = False
+
         m1 = ctx.re1.match(line)
-        if m1:
-            w1 = int(m1.group(1), base=0)
-            w2 = int(m1.group(2), base=0)
-            ctx.mapping[w1] = w2
+
+        if (len(line)==0) or (line[0:1]=="#"):
+            flag = True
+        if not flag:
+            if m1:
+                w1 = int(m1.group(1), base=0)
+                w2 = int(m1.group(2), base=0)
+                ctx.mapping[w1] = w2
+                flag = True
+        if not flag:
+            m1 = ctx.re2.match(line)
+            if m1:
+                w1 = int(m1.group(1), base=0)
+                w2 = int(m1.group(2), base=16)
+                ctx.mapping[w1] = w2
+                flag = True
+        if not flag:
+            m1 = ctx.re_1to2.match(line)
+            if m1:
+                w1 = int(m1.group(1), base=0)
+                w2 = int(m1.group(2), base=16)
+                w3 = int(m1.group(3), base=16)
+                ctx.mapping[w1] = map_1to2_hack(ctx, w2, w3)
+                flag = True
+        if not flag:
+            m1 = ctx.re_undef.match(line)
+            if m1:
+                flag = True
+        if not flag:
+            raise Exception("Failed to parse line [%s]" % (line))
+
+# In my tests, this XOR filter reduced the size by a disappointing
+# 5.0% -- less than 1000 bytes.
+# (I suppose there does exist a way to procedurally generate some
+# parts of the data, or to arrange and filter it to make it more
+# compressible.)
+def filter_uncblob(ctx):
+    tmpblob = ctx.uncblob
+    ctx.uncblob = bytearray(len(tmpblob))
+
+    prev = 0
+    for i in range(len(tmpblob)):
+        if i==0:
+            ctx.uncblob[i] = tmpblob[i]
+        else:
+            ctx.uncblob[i] = tmpblob[i-1] ^ tmpblob[i]
 
 def mapping_to_uncblob(ctx):
-    ctx.uncblob = bytearray(65536)
+    ctx.uncblob = bytearray(g_num_dst_codes)
     for i in range(32768):
-        ctx.uncblob[i] = ctx.mapping[32768+i] >> 8
-        ctx.uncblob[32768+i] = ctx.mapping[32768+i] & 0xff
+        ctx.uncblob[i] = (ctx.mapping[32768+i] >> 16) & 0xff
+        ctx.uncblob[32768+i] = (ctx.mapping[32768+i] >> 8) & 0xff
+        ctx.uncblob[65536+i] = ctx.mapping[32768+i] & 0xff
 
 def write_c_file(ctx, outfn, is_compressed):
     if is_compressed:
@@ -51,11 +111,14 @@ def write_c_file(ctx, outfn, is_compressed):
         outf.write("// This data is zlib-compressed. After decompression,\n" \
             "// it is a CP932-to-Unicode lookup table for the double-byte\n" \
             "// CP932 characters, from 0x8000 to 0xffff.\n" \
-            "// For compressibility reasons, first it has 32768 high bytes,\n" \
-            "// then 32768 low bytes.\n" \
+            "// For compressibility reasons, it is organized as:\n" \
+            "// - 32768 \"plane\" or special bytes\n" \
+            "// - 32768 high bytes\n" \
+            "// - 32768 low bytes\n" \
             "// For more information, see the \"deark-extras\" project\n" \
             "// listed in the main Deark readme file.\n\n")
 
+    outf.write("#define DE_CP932DATA_ORIG_LEN %d\n" % (len(ctx.uncblob)))
     outf.write("#define DE_CP932DATA_LEN %d\n" % (len(blob)))
     outf.write("static const u8 de_cp932data[DE_CP932DATA_LEN] = {")
     for i in range(0, len(blob)):
@@ -66,8 +129,10 @@ def write_c_file(ctx, outfn, is_compressed):
 
         if not is_compressed:
             if i==0:
-                outf.write("// high bytes\n\t")
+                outf.write("// plane, etc., bytes\n\t")
             elif i==32768:
+                outf.write("// high bytes\n\t")
+            elif i==65536:
                 outf.write("// low bytes\n\t")
 
         outf.write(str(blob[i]))
@@ -89,7 +154,7 @@ def write_cmpr_data_file(ctx, outfn):
     outf.close()
 
 def dump_mapping(ctx):
-    for i in range(65536):
+    for i in range(g_num_src_codes):
         print("item 0x%x 0x%x" % (i, ctx.mapping[i]))
 
 def main():
@@ -101,7 +166,7 @@ def main():
 
     ctx.fn = sys.argv[1]
 
-    ctx.mapping = [0] * 65536
+    ctx.mapping = [0] * g_num_src_codes
 
     ctx.inf = open(ctx.fn, "r", encoding='utf8', newline='\n')
     read_mapping_file(ctx)
@@ -111,6 +176,9 @@ def main():
         dump_mapping(ctx)
 
     mapping_to_uncblob(ctx)
+
+    if ctx.use_xor_filter:
+        filter_uncblob(ctx)
 
     if ctx.debugmode:
         write_unc_data_file(ctx, "cp932data_u.dat")
--- cp932/readme.txt
+++ cp932/readme.txt
@@ -3,18 +3,16 @@ table, in the form of a C header file.
 
 Typical usage:
 
-  ./cp932.py CP932.TXT
+  ./cp932.py sjis-0213-2004-std.txt
 
 This creates a file named cp932data.h.
 
-Caution: Additional logic is needed to handle single-byte characters, and to
-handle different variants of "code page 932" and "Shift-JIS".
+Caution: Additional logic is needed to handle single-byte characters, codes
+that map to multiple Unicode codepoints, and to handle different variants of
+"code page 932" and "Shift-JIS".
 
-CP932.TXT was retrieved from
+The sjis-0213-2004-std.txt file was retrieved from
+  https://x0213.org/codetable/sjis-0213-2004-std.txt
+
+This script was originally designed to work with CP932.TXT from
   https://www.unicode.org/Public/MAPPINGS/VENDORS/MICSFT/WINDOWS/CP932.TXT
-
-The CP932.TXT file might be subject to the terms of use in UNICODE LICENSE V3,
-reproduced in LICENSE-UNICODE.txt.
-
-LICENSE-UNICODE.txt was retrieved from
-  https://www.unicode.org/license.txt
